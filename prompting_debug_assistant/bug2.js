56911c96ca732c346a2a727b52f55685ab6dfee6
Vusale-Holberton <vusaleeliyeva187@gmail.com>
2026-05-11 01:44:15 +0400

    fix: restore snippets folder structure

--- prompting_debug_assistant/bug2.js
+++
@@ -1,7 +0,0 @@
-function calculateTotal(price, tax) {
-    /* Bu funksiya cəmi hesablamalıdır 
-       amma sintaksis xətası var */
-    console.log("Total is: " + (price + tax)
-}
-
-calculateTotal(100, 18);
