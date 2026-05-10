961c92a6b527b105ffe710b82376cf3e59e71f1c
Vusale-Holberton <vusaleeliyeva187@gmail.com>
2026-05-11 02:03:54 +0400

    cleaned up snippets

--- prompting_debug_assistant/snippets/bug3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Tip xətası: int dəyişəninə string mətni mənimsədilir
-    int result = "Xeta mesaji";
-    cout << result << endl;
-    return 0;
-}
