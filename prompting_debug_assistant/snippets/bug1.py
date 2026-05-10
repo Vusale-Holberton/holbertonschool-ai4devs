81ee9cb084c62aa2b47aeb63c16019dfe6618004
Vusale-Holberton <vusaleeliyeva187@gmail.com>
2026-05-11 01:51:49 +0400

    final folder structure fix

---
+++ prompting_debug_assistant/snippets/bug1.py
@@ -0,0 +1,7 @@
+def greet_user(name):
+    # Bu funksiya istifadəçini salamlayır
+    # Amma mötərizə bağlanmayıb
+    print("Hello, " + name
+
+greet_user("Vusala")
+# Kodun sətir sayını artırmaq üçün şərh yazırıq
---
+++ prompting_debug_assistant/snippets/bug3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Tip xətası: int dəyişəninə string mətni mənimsədilir
+    int result = "Xeta mesaji";
+    cout << result << endl;
+    return 0;
+}
